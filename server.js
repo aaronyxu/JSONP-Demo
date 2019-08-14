355f7766ee72602bd72bdf2bb6d66287c2b04395
aaronyxu <52167498+aaronyxu@users.noreply.github.com>
2019-08-14 21:53:39 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,64 @@
+
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+
+
+    console.log('含查询字符串的路径\n' + pathWithQuery)
+
+    if (path === '/') {
+        var string = fs.readFileSync('./index.html','utf8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end(string)
+    } else if (path === '/style') {
+        var string = fs.readFileSync('./style.css','utf8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
+        response.end(string)
+    } else if (path === '/main') {
+        var string = fs.readFileSync('./main.js','utf8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.end(string)
+    } else if (path === '/pay') {
+        let amount = fs.readFileSync('./db', 'utf8')
+        amount -= 1
+        fs.writeFileSync('./db', amount)
+        let callbackName = query.callback
+        response.setHeader('Content-Type', 'application/javascript')
+        response.statusCode = 200
+        response.write(`
+            ${callbackName}.call(undefined, 'success')
+        `)
+        response.end()
+    }
+
+    else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write('找不到相应路径')
+        response.end()
+    }
+
+
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请打开 http://localhost:' + port)
