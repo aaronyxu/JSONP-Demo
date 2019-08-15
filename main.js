978abb1d37ad2dcc5a9e7a4105fe789f43d13e37
aaronyxu <52167498+aaronyxu@users.noreply.github.com>
2019-08-15 21:52:03 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+button.addEventListener('click', (e)=>{
+    $.ajax({
+        url: "http://jack.com:8002/pay",
+        dataType: "jsonp",
+        success: function( response ) {
+            if(response === 'success'){
+                amount.innerText = amount.innerText - 1
+            }
+        }
+    })
+})
